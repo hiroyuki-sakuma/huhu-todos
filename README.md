85ef542df95daeadd52c6b9e90f595c1fbb58f40
hiroyuki.sakuma <hiroyuki.sakuma@rightcode.co.jp>
2024-10-12 13:57:27 +0900

    環境構築

---
+++ README.md
@@ -0,0 +1,15 @@
+# 構成
+## バックエンド
+PHP、Apache
+## DB
+MySQL
+## フロント
+React、MUI、Vite、TypeScript、Biome
+
+※今後やる
+## CI/CD
+GitHub Actions
+## テスト
+Jest、PHPUnit
+## インフラ
+herokuかGCP？
---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import viteLogo from "/vite.svg";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/zen-kaku-gothic-new/300.css";
+import "@fontsource/zen-kaku-gothic-new/400.css";
+import "@fontsource/zen-kaku-gothic-new/500.css";
+import "@fontsource/zen-kaku-gothic-new/700.css";
+import Button from "@mui/material/Button";
+
+function App() {
+	return (
+		<div>
+			home <br />
+			<Button >Hello world</Button>
+		</div>
+	);
+}
+
+export default App;
